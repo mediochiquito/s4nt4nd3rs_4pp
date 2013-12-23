19a1c02d86f847f66b69a889d32914d27caf33e3
mediochiquito <mateomenrstrina@MacBook-Pro-de-Mateo.local>
2013-12-23 18:16:35 -0200

    sdfdsf

--- www/js/com/utils/Facebook.js
+++ www/js/com/utils/Facebook.js
@@ -17,29 +17,29 @@ function Facebook(){
 
      	 FB.getLoginStatus(function(response) {
 	          	if (response.status == 'connected') {
-
+/*
 	             		if ( device.platform == 'android' || device.platform == 'Android' )
 							alert(response.authResponse.userId +'__'+ response.authResponse.accessToken);
 						else 
 							alert(response.authResponse.userID +'__'+ response.authResponse.accessToken);
-
-
+*/
 						$callback();
+
 					} else {
 			
 			             FB.login(function(response) {
 					 		  console.log(response)
 							  if (response.authResponse) {
 							    	
-							    	if ( device.platform == 'android' || device.platform == 'Android' )
+							    	/*if ( device.platform == 'android' || device.platform == 'Android' )
 										 alert(response.authResponse.userId +'__'+ response.authResponse.accessToken);
-									else alert(response.authResponse.userID +'__'+ response.authResponse.accessToken);
+									else alert(response.authResponse.userID +'__'+ response.authResponse.accessToken);*/
 							    	$callback();
-							    	
+
 							   } else {
 							     alert('User cancelled login or did not fully authorize.');
 							   }
-							 }, {scope: ''})
+						}, {scope: ''})
 	         		}
          });
 
