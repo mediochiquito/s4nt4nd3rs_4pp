736f678fccd3202f0a985870ec9463b63bd81f4d
mediochiquito <mateomenrstrina@MacBook-Pro-de-Mateo.local>
2013-12-26 12:53:33 -0200

    asd

--- server/json.participaciones.php
+++ server/json.participaciones.php
@@ -2,6 +2,7 @@
 include dirname(__FILE__) . '/_init.php';
 $id_evento = $_POST['id_evento'];
 $rs = mysql_query('SELECT * FROM participaciones WHERE participaciones_eventos_id="' . $id_evento . '" ORDER BY participaciones_id DESC');
+<<<<<<< HEAD
         
 
         $rows = array();
@@ -9,4 +10,14 @@ $rs = mysql_query('SELECT * FROM participaciones WHERE participaciones_eventos_i
                  $rows[] = $row;
         }
         echo json_encode($rows);
+=======
+	
+
+	$rows = array();
+	while($row =  mysql_fetch_assoc($rs)){
+		 $rows[] = $row;
+	}
+	echo json_encode($rows);
+>>>>>>> 5f4096a256e72ee7fd24c0eadb1cb06f8aba01c2
 ?>
+
