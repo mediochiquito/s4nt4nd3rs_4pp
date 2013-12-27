7a182a48b66b23f70ba3d433515a7ada1b1ca230
mediochiquito <mateomenrstrina@MacBook-Pro-de-Mateo.local>
2013-12-27 17:20:12 -0200

    asd

--- www/js/com/header/Header.js
+++ www/js/com/header/Header.js
@@ -18,6 +18,8 @@ function Header(){
 	search.type = 'text';
 	$(this.main).append(search);
 	$(search).bind('keyup', doKeyUp)
+	$(search).bind('click', do_click)
+	$(search).bind('blur', do_blur)
 	var btn_close = new Boton2Frames('img/header/close.svg', 30, 60, do_click_btn_close)
 	btn_close.main.id = 'Header_btn_close'
 	$(this.main).append(btn_close.main)
@@ -27,21 +29,39 @@ function Header(){
 	if(ancho_search>270) ancho_search = 270;
 	$(search).css({'width':ancho_search});
 
+	var wm = 'Buscar...';
+	$(search).val(wm);
+	function do_blur(){
+
+		if($(search).val() == '') $(search).val(wm);
+	}
 
 	function doKeyUp(e){
-		if(app.secciones.get_obj_seccion_actual().main.id != 'SeccionEventosOfertas')
-		app.secciones.go(app.secciones.seccioneventosofertas);
-		
-		if($(search).val().length > 0) $(btn_close.main).show();
 
-		app.secciones.seccioneventosofertas.cargar_listas($(search).val())
+		if(app.secciones.get_obj_seccion_actual().main.id != 'SeccionEventosOfertas');
+		app.secciones.go(app.secciones.seccioneventosofertas);
+		if($(search).val().length > 0) $(btn_close.main).show();
+		app.secciones.seccioneventosofertas.cargar_listas($(search).val());
 
 	}
+
+	function do_click(){
+		
+		if($(search).val() == wm) $(search).val('');
+
+	}
+
 	function do_click_btn_close(){
 
 		$(search).val('');
 		$(search).blur();
 		$(btn_close.main).hide()
+		app.secciones.seccioneventosofertas.cargar_listas('')
+
+		setTimeout(function(){
+			$(search).blur();
+		}, 200);
+		
 
 	}
 	function doMenu(){
