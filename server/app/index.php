cb8289608fc1774de0564fa8e83e407d8aaeb7fe
mediochquito <mediochquito@gmail.com>
2014-01-02 02:19:33 -0200

    sadasd

--- server/app/index.php
+++ server/app/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>GreenRay Led Apps</title>
+<title>SantandersApp</title>
 <style>
 
 div{
@@ -24,7 +24,7 @@ div{
 <div><img src='icon.png' /></div>
 
 <div><a href='itms-services://?action=download-manifest&url=http://<?=preg_replace('~www.~i', '', $_SERVER['HTTP_HOST'])?>/app/app.plist'>Installar App para iOS</a></div>
-<div><a href='SantanderApp.apk'>Installar App para Android</a></div>
+<div><a href='SantandersApp.apk'>Installar App para Android</a></div>
 
 
 </center>
